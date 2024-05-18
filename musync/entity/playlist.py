f141af2eb91012796beb502a2489c7a83ed1567f
sacklippe <jonas.kleppich@voltfang.de>
2024-05-18 19:24:48 +0200

    refactor: store user id instead of user with playlist entity

--- musync/entity/playlist.py
+++ musync/entity/playlist.py
@@ -6,13 +6,13 @@ from dataclasses import dataclass
 @dataclass
 class Playlist:
     playlist_id: str
+    owner_id: str
     name: str
-    owner: str
 
     @classmethod
     def from_spotify(cls, playlist: dict) -> Playlist:
         return cls(
             playlist_id=playlist["id"],
+            owner_id=playlist["owner"]["id"],
             name=playlist["name"],
-            owner=playlist["owner"]["id"],
         )
