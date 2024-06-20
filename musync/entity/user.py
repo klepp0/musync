9038f2a5c6509117eea102700d551fad142aaab5
sacklippe <jonas.kleppich@voltfang.de>
2024-06-20 16:50:06 +0200

    feat: init user from tidal

--- musync/entity/user.py
+++ musync/entity/user.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+import tidalapi as tidal
+
 
 @dataclass
 class User:
@@ -13,4 +15,11 @@ class User:
         return cls(
             user_id=user["id"],
             name=user["display_name"],
+        )
+
+    @classmethod
+    def from_tidal(cls, user: tidal.LoggedInUser) -> User:
+        return cls(
+            user_id="" if user.id is None else str(user.id),
+            name="" if user.username is None else user.username,
         )
