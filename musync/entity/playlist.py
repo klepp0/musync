92eebf73c17635d546b5d8e884f5a00eec4fa1dc
sacklippe <jonas.kleppich@voltfang.de>
2024-06-20 16:02:18 +0200

    feat: load from tidal

--- musync/entity/playlist.py
+++ musync/entity/playlist.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+import tidalapi as tidal
+
 
 @dataclass
 class Playlist:
@@ -17,4 +19,17 @@ class Playlist:
             owner_id=playlist["owner"]["id"],
             name=playlist["name"],
             n_tracks=playlist["tracks"]["total"],
+        )
+
+    @classmethod
+    def from_tidal(cls, playlist: tidal.Playlist) -> Playlist:
+        return cls(
+            playlist_id="" if playlist.id is None else playlist.id,
+            owner_id=(
+                ""
+                if playlist.creator is None or playlist.creator.id is None
+                else playlist.creator.id
+            ),
+            name="" if playlist.name is None else playlist.name,
+            n_tracks=playlist.num_tracks,
         )
--- musync/entity/track.py
+++ musync/entity/track.py
@@ -2,13 +2,15 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+import tidalapi as tidal
+
 
 @dataclass
 class Track:
     track_id: str
     artist_id: str
     name: str
-    date_added: str  # relates to playlist, requires better structure
+    date_added: Optional[str]  # relates to playlist, requires better structure
 
     @classmethod
     def from_spotify(cls, track: dict) -> Track:
@@ -17,4 +19,17 @@ class Track:
             artist_id=track["track"]["artists"][0]["id"],
             name=track["track"]["name"],
             date_added=track["added_at"],
+        )
+
+    @classmethod
+    def from_tidal(cls, track: tidal.Track) -> Track:
+        return cls(
+            track_id="" if track.id is None else track.id,
+            artist_id=(
+                ""
+                if track.artist is None or track.artist.id is None
+                else track.artist.id
+            ),
+            name="" if track.name is None else track.name,
+            date_added="" if track.user_date_added is None else str(track.user_date_added),
         )
