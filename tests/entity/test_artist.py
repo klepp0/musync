5d0dc0bc0bd5bbdea29c70b0262f20c0025e7313
sacklippe <jonas.kleppich@voltfang.de>
2024-06-21 20:21:08 +0200

    test: add test cases for spotify and tidal artist

---
+++ tests/entity/test_artist.py
@@ -0,0 +1,35 @@
+import pickle
+from datetime import datetime as dt
+from pathlib import Path
+
+import pytest
+
+from musync.entity import Artist
+
+DATA_DIR = Path(__file__).parent / "data"
+
+
+@pytest.fixture
+def spotify_artist():
+    with open(DATA_DIR / "test_artist_spotify.pkl", "rb") as f:
+        return pickle.load(f)
+
+
+@pytest.fixture
+def tidal_artist():
+    with open(DATA_DIR / "test_artist_tidal.pkl", "rb") as f:
+        return pickle.load(f)
+
+
+def test_artist_from_spotify(spotify_artist):
+    artist = Artist.from_spotify(spotify_artist)
+
+    assert artist.artist_id == "3jOstUTkEu2JkjvRdBA5Gu"
+    assert artist.name == "Weezer"
+
+
+def test_artist_from_tidal(tidal_artist):
+    artist = Artist.from_tidal(tidal_artist)
+
+    assert artist.artist_id == "18272666"
+    assert artist.name == "Vodoom"
