5bb7fb3e6a4ffe05d9d92ee7a4a53277dfbae682
sacklippe <jonas.kleppich@voltfang.de>
2024-06-21 20:19:51 +0200

    feat: load artist from tidal

--- musync/entity/artist.py
+++ musync/entity/artist.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+import tidalapi as tidal
+
 
 @dataclass
 class Artist:
@@ -13,4 +15,11 @@ class Artist:
         return cls(
             artist_id=artist["id"],
             name=artist["name"],
+        )
+
+    @classmethod
+    def from_tidal(cls, artist: tidal.Artist) -> Artist:
+        return cls(
+            artist_id="" if artist.id is None else str(artist.id),
+            name="" if artist.name is None else artist.name,
         )
