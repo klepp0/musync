f6bd442e48839105b5a812a92be40c5a51ee74c2
sacklippe <jonas.kleppich@voltfang.de>
2024-06-24 21:45:54 +0200

    test: tidal session

---
+++ tests/integrationtests/tidal/test_session.py
@@ -0,0 +1,21 @@
+import pytest
+
+from musync.entity import User, Playlist
+from musync.tidal import TidalSession
+
+
+@pytest.fixture
+def tidal_session():
+    return TidalSession()
+
+
+def test_session_is_logged_in(tidal_session):
+    assert tidal_session.check_login()
+
+
+def test_session_user(tidal_session):
+    assert isinstance(tidal_session.user, User)
+
+
+def test_session_get_playlists(tidal_session):
+    assert all(isinstance(p, Playlist) for p in tidal_session.get_playlists())
