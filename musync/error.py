11af2480aa5ef11de948820721dbddf38e6ee55c
sacklippe <jonas.kleppich@voltfang.de>
2024-07-04 14:46:16 +0200

    feat: add custom error for missing priviliges and incompatible entities

--- musync/error.py
+++ musync/error.py
@@ -1,2 +1,10 @@
 class PlaylistNotFoundError(Exception):
     """Raised when a playlist is not found in the database."""
+
+
+class MissingPrivilegesError(Exception):
+    """Raised when a user does not have the necessary privileges."""
+
+
+class IncompatibleEntityError(Exception):
+    """Raised when an entity is not compatible with the operation."""
