373eb43784fa23a96dd5affb55447ce853ee9dc6
sacklippe <jonas.kleppich@voltfang.de>
2024-07-04 14:50:00 +0200

    test: integration test for adding to tidal playlist

--- tests/integrationtests/tidal/test_session.py
+++ tests/integrationtests/tidal/test_session.py
@@ -1,8 +1,9 @@
+import datetime
 import pickle
 
 import pytest
 
-from musync.entity import Playlist, Track, User
+from musync.entity import Origin, Playlist, Track, User
 from musync.tidal import TidalSession
 from tests.unittests.entity import DATA_DIR
 
@@ -16,6 +17,52 @@ def tidal_session():
 def spotify_track():
     with open(DATA_DIR / "test_track_spotify.pkl", "rb") as f:
         return Track.from_spotify(pickle.load(f))
+
+
+@pytest.fixture
+def tidal_track():
+    with open(DATA_DIR / "test_track_tidal.pkl", "rb") as f:
+        return Track.from_tidal(pickle.load(f))
+
+
+@pytest.fixture
+def tidal_track_list():
+    tracks = [
+        Track(
+            track_id="338383176",
+            artist_ids=["7343085"],
+            name="Cabalero",
+            duration=datetime.timedelta(seconds=345),
+            origin=Origin.TIDAL,
+        ),
+        Track(
+            track_id="304574491",
+            artist_ids=["10644748", "6576000"],
+            name="Tension",
+            duration=datetime.timedelta(seconds=263),
+            origin=Origin.TIDAL,
+        ),
+        Track(
+            track_id="121277192",
+            artist_ids=["15910495"],
+            name="Overdub (Original Mix)",
+            duration=datetime.timedelta(seconds=348),
+            origin=Origin.TIDAL,
+        ),
+    ]
+    return tracks
+
+
+@pytest.fixture
+def create_playlist(tidal_session):
+    title = "Test Playlist"
+    description = "This playlist was created by https://github.com/klepp0/musync"
+    new_tidal_playlist = tidal_session._client.user.create_playlist(title, description)
+    new_playlist = Playlist.from_tidal(new_tidal_playlist)
+
+    yield new_playlist
+
+    tidal_session._client.playlist(new_playlist.playlist_id).delete()
 
 
 def test_session_is_logged_in(tidal_session):
@@ -33,3 +80,15 @@ def test_session_load_playlists(tidal_session):
 def test_search_track(tidal_session, spotify_track):
     tidal_track = tidal_session.find_track(spotify_track)
     assert spotify_track.equals(tidal_track)
+
+
+def test_add_to_playlist(tidal_session, create_playlist, tidal_track_list):
+    playlist = create_playlist
+    n_tracks_before = playlist.n_tracks
+    tidal_session.add_to_playlist(playlist, tidal_track_list)
+
+    updated_tidal_playlist = tidal_session._client.playlist(playlist.playlist_id)
+    updated_playlist = Playlist.from_tidal(updated_tidal_playlist)
+    n_tracks_after = updated_playlist.n_tracks
+
+    assert n_tracks_before < n_tracks_after
