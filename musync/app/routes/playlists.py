7add1de056e2eb8b3a216194f8d00fa55b527934
sacklippe <jonas.kleppich@voltfang.de>
2024-07-08 11:52:08 +0200

    refactor: simplify variable types

--- musync/app/routes/playlists.py
+++ musync/app/routes/playlists.py
@@ -2,7 +2,6 @@ import logging
 from typing import List, Literal, Optional
 
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
 
 # from musync.common.entity import Playlist
 from musync.app.models import Playlist, Track
@@ -66,18 +65,37 @@ def get_playlist_tracks(
             )
 
     playlist = session.load_playlist(playlist_id)
+    if not playlist:
+        return None
 
     return session.load_playlist_tracks(playlist)
 
 
 ### POST ###
-class PlaylistCreateRequest(BaseModel):
-    title: str
-
-
 @router.post("/playlists/{origin}/", response_model=Playlist)
-def create_playlist(
-    payload: PlaylistCreateRequest, origin: Literal["spotify", "tidal"]
+def create_playlist(title: str, origin: Literal["spotify", "tidal"]) -> Playlist:
+    match origin:
+        case "spotify":
+            session = SpotifySession()
+        case "tidal":
+            session = TidalSession()
+        case _:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Origin must be either 'spotify' or 'tidal' ({origin=}).",
+            )
+
+    new_playlist = session.create_playlist(title)
+    logger.info(f"Created playlist: {new_playlist}")
+
+    return new_playlist
+
+
+@router.post("/playlists/{origin}/{playlist_id}/tracks", response_model=Playlist)
+def add_tracks_to_playlist(
+    playlist_id: str,
+    origin: Literal["spotify", "tidal"],
+    track_ids: List[str],
 ) -> Playlist:
     match origin:
         case "spotify":
@@ -90,25 +108,14 @@ def create_playlist(
                 detail=f"Origin must be either 'spotify' or 'tidal' ({origin=}).",
             )
 
-    new_playlist = session.create_playlist(payload.title)
-    logger.info(f"Created playlist: {new_playlist}")
+    playlist = session.load_playlist(playlist_id)
+    if not playlist:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Playlist with ID {playlist_id} not found.",
+        )
 
-    return new_playlist
-
-
-@router.post("/playlists/{origin}/{playlist_id}/tracks", response_model=Playlist)
-def add_tracks_to_playlist(tracks: List[Track], origin: Literal["spotify", "tidal"]):
-    match origin:
-        case "spotify":
-            session = SpotifySession()
-        case "tidal":
-            session = TidalSession()
-        case _:
-            raise HTTPException(
-                status_code=400,
-                detail=f"Origin must be either 'spotify' or 'tidal' ({origin=}).",
-            )
-
+    tracks = [session.load_track(_id) for _id in track_ids]
     updated_playlist = session.add_to_playlist(playlist, tracks)
 
     logger.info(f"Added {tracks=} to playlist: {updated_playlist}")
